6ff7dd7f3d7cd375fee8fc96a10288ed42eed39c
Peter McDaniel <kg.mcd@hotmail.com>
2016-07-10 01:02:44 +0200

    Update to use the GUI

--- ListCreator.py
+++ ListCreator.py
@@ -6,6 +6,7 @@ playlistNames = ['Monstercat - Future Bass',
                  'Monstercat - Electro',
                  'Monstercat - Indie Dance',
                  'Monstercat - Glitch Hop',
+                 'Monstercat - House',
                  'NCS - House',
                  'NCS - Drum & Bass',
                  'xKito - Drum & Bass',
@@ -15,12 +16,14 @@ playlistNames = ['Monstercat - Future Bass',
                  'xKito - Glitch Hop',
                  'Vexento - Electro',
                  'Vexento - Inspiration',
-                 'Vexento - Chill']
+                 'Vexento - Chill',
+                 'Artzie']
 urlList = ['https://www.youtube.com/playlist?list=PLe8jmEHFkvsbRwwi0ode5c9iMQ2dyJU3N',
            'https://www.youtube.com/playlist?list=PL9BCA60EEB1C8893D',
            'https://www.youtube.com/playlist?list=PL21A7A915E7020E73',
            'https://www.youtube.com/playlist?list=PLe8jmEHFkvsZWIBJEqIhsu2VgDI5w7r0d',
            'https://www.youtube.com/playlist?list=PLe8jmEHFkvsY-hyGZb1POOp8GDdIRng5A',
+           'https://www.youtube.com/playlist?list=PLDF056FBA2E172F87',
            'https://www.youtube.com/playlist?list=PLRBp0Fe2GpgmsW46rJyudVFlY6IYjFBIK',
            'https://www.youtube.com/playlist?list=PLRBp0Fe2GpgnzYdHtTCoBYPyIJG9_opMn',
            'https://www.youtube.com/playlist?list=PLvlw_ICcAI4fZQ5IS-AaTEnsTHy0zuXr8',
@@ -30,7 +33,8 @@ urlList = ['https://www.youtube.com/playlist?list=PLe8jmEHFkvsbRwwi0ode5c9iMQ2dy
            'https://www.youtube.com/playlist?list=PLvlw_ICcAI4dgellaUrmAcV7s-VhfqHat',
            'https://www.youtube.com/playlist?list=PLcd3emSF7UMCo1PBgnOGjbA6V6AhCb2nB',
            'https://www.youtube.com/playlist?list=PLcd3emSF7UMDKfLXUHv9a5nUAU2ogqn42',
-           'https://www.youtube.com/playlist?list=PLcd3emSF7UMCEVznCb2foDghKoq7ehcD1']
+           'https://www.youtube.com/playlist?list=PLcd3emSF7UMCEVznCb2foDghKoq7ehcD1',
+           'https://www.youtube.com/playlist?list=PLWvkIZpd20Tyq9xOXW9qb_lcZ7TSbVGpa']
 
 playlists = []
 for a,b in zip(playlistNames,urlList):
@@ -56,26 +60,22 @@ def createFile():
 def dlSongs():
     for a in playlists:
         for song in a.diff():
-            os.system('youtube-dl -x --audio-format mp3 -o "%s - %%(title)s-%%(id)s.%%(ext)s" %s' %
+            print("%s" % song)
+        for song in a.diff():
+            os.system('youtube-dl -x -i --audio-format mp3 -o "%s - %%(title)s-%%(id)s.%%(ext)s" %s' %
                   (a.name, song))
 def dlSongsDate(date):
     for a in playlists:
-        os.system('youtube-dl -x --audio-format mp3 --dateafter %s -o "%s - %%(title)s-%%(id)s.%%(ext)s" %s' %
+        os.system('youtube-dl -x --audio-format mp3 -i --dateafter %s -o "%s - %%(title)s-%%(id)s.%%(ext)s" %s' %
                   (date,a.name, a.listURL))
 def updateCSVFiles():
     for a in playlists:
         a.updateCSVFile()
 
-def CSVtoTXT():
-    for playlist in playlists:
-        file = open('%s.txt'%(playlist.name), 'w')
-        for b in a.oldList():
-            x,s = b.split('=')
-            file.write('youtube {}'.format(s))
-
 def pickle():
     for a in playlists:
         a.write()
+
 def test_pickle():
     for a in playlists:
         print(a.oldList())
